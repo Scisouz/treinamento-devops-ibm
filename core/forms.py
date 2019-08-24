97923800386e76749bfdef033d41bdae50f83e64
Scisouz <fisouz@gmail.com>
2019-08-24 22:02:28 +0000

    This closes #1 - remover campo data

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,4 @@ from .models import Todo
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        widgets = {
-            'due_date': forms.DateInput(attrs={'class':'datepicker'})
-        }
-        fields = ('title', 'text', 'due_date', 'completed')
+        fields = ('title', 'text', 'completed')
